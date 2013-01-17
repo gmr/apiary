f01be94f9ed99b5772513b92100f610a9d14ca07
Gavin M. Roy <gmr@meetme.com>
2013-01-17 15:53:01 -0500

    Update distribution to use new_query and add API for architecture and breed

--- apiary/api/__init__.py
+++ apiary/api/__init__.py
@@ -1,9 +1,17 @@
 __author__ = 'gmr'
 
+from apiary.api import architecture
+from apiary.api import breed
 from apiary.api import distribution
 
 UUID_ID = "(?P<id>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})"
 
-ROUTES = [("/api/distribution", distribution.Distribution),
+ROUTES = [("/api/architecture", architecture.Architecture),
+          ("/api/architecture/(?P<name>[\w_\-]*)", architecture.Architecture),
+          ("/api/architectures", architecture.Architectures),
+          ("/api/breed", breed.Breed),
+          ("/api/breed/(?P<name>[\w_\-]*)", breed.Breed),
+          ("/api/breeds", breed.Breeds),
+          ("/api/distribution", distribution.Distribution),
           ("/api/distribution/%s" % UUID_ID, distribution.Distribution),
           ("/api/distributions", distribution.Distributions)]
--- apiary/api/architecture.py
+++ apiary/api/architecture.py
@@ -1,89 +1,89 @@
 """
-API Distribution related Request Handlers
+Architecture API related Request Handlers
 
 """
 from apiary.api import base
-from apiary.models import distribution
+from apiary.models import architecture
 import logging
 
 LOGGER = logging.getLogger(__name__)
 
 
-class Distribution(base.RequestHandler):
-    """API interface for managing distribution data"""
+class Architecture(base.RequestHandler):
+    """API interface for managing architecture data"""
 
-    JSONP_METHOD = 'on_distribution'
+    JSONP_METHOD = 'on_architecture'
 
-    def fetch_distribution(self, kwargs):
-        """Get a single distribution from the database.
+    def fetch_architecture(self, kwargs):
+        """Get a single architecture from the database.
 
         :param dict kwargs: Keyword arguments from the request
-        :rtype: apiary.models.distribution.Distribution
+        :rtype: apiary.models.architecture.Architecture
 
         """
-        dist_id = self.parameter_value('id', kwargs)
-        if dist_id:
-            return self.new_query(distribution.Distribution).get(dist_id)
+        name = self.parameter_value('name', kwargs)
+        if name:
+            return self.new_query(architecture.Architecture).get(name)
 
     def delete(self, *args, **kwargs):
-        """Delete a distribution from the system. Accepts distribution_id as a
+        """Delete a architecture from the system. Accepts architecture_id as a
         query parameter or in the URI
 
         :param list args: Positional arguments
         :param dict kwargs: Keyword arguments
 
         """
-        value = self.fetch_distribution(kwargs)
+        value = self.fetch_architecture(kwargs)
         if not value:
             return self.set_status(404)
-        LOGGER.warning('Deleting distribution %s', value.id)
+        LOGGER.warning('Deleting architecture %s', value.id)
         self.database.delete(value)
         self.database.commit()
         self.set_status(204)
 
     def get(self, *args, **kwargs):
-        """Return the details for a distribution from the system. Accepts
-        distribution_id as a query parameter or in the URI
+        """Return the details for a architecture from the system. Accepts
+        architecture_id as a query parameter or in the URI
 
         :param list args: Positional arguments
         :param dict kwargs: Keyword arguments
 
         """
-        value = self.fetch_distribution(kwargs)
+        value = self.fetch_architecture(kwargs)
         if not value:
             return self.set_status(404)
         self.write_mapping(value)
 
     def post(self, *args, **kwargs):
-        """Insert or update a distribution in the system.
+        """Insert or update a architecture in the system.
 
         :param list args: Positional arguments
         :param dict kwargs: Keyword arguments
 
         """
-        value = self.fetch_distribution(kwargs)
+        value = self.fetch_architecture(kwargs)
 
         # Insert
         if not value:
-            distro = distribution.Distribution()
+            distro = architecture.Architecture()
             self.assign_attributes(distro, kwargs)
             self.database.add(distro)
             self.database.commit()
-            LOGGER.info('Added new distribution: %r', distro)
+            LOGGER.info('Added new architecture: %r', distro)
             return self.set_status(201)
 
         # Update
         self.assign_attributes(value, kwargs)
         self.database.update(value)
         self.database.commit()
-        LOGGER.info('Updated distribution: %r', value)
+        LOGGER.info('Updated architecture: %r', value)
         return self.set_status(204)
 
 
-class Distributions(base.RequestHandler):
-    """API interface for fetching a list of distributions"""
+class Architectures(base.RequestHandler):
+    """API interface for fetching a list of architectures"""
 
-    JSONP_METHOD = 'on_distributions'
+    JSONP_METHOD = 'on_architectures'
 
     def get(self, *args, **kwargs):
         """Return a list of definitions from the system. Optional query
@@ -97,4 +97,4 @@ class Distributions(base.RequestHandler):
         :param dict kwargs: Keyword arguments
 
         """
-        self.write_query_results(distribution.Distribution)
+        self.write_query_results(architecture.Architecture)
