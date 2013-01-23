bc9f272c250f67af489bc61f13dfc1fc8a5dc623
Gavin M. Roy <gmr@meetme.com>
2013-01-22 22:41:42 -0500

    Add API URI mappings

--- apiary/api/__init__.py
+++ apiary/api/__init__.py
@@ -3,6 +3,7 @@ __author__ = 'gmr'
 from apiary.api import architecture
 from apiary.api import breed
 from apiary.api import distribution
+from apiary.api import system
 
 UUID_ID = "(?P<id>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})"
 
@@ -14,4 +15,7 @@ ROUTES = [("/api/architecture", architecture.Architecture),
           ("/api/breeds", breed.Breeds),
           ("/api/distribution", distribution.Distribution),
           ("/api/distribution/%s" % UUID_ID, distribution.Distribution),
-          ("/api/distributions", distribution.Distributions)]
+          ("/api/distributions", distribution.Distributions),
+          ("/api/system", system.System),
+          ("/api/system/%s" % UUID_ID, system.System),
+          ("/api/systems", system.Systems)]
