b4f515f2d5ebddfc460c6a57deb71c2e7eb77e73
Gavin M. Roy <gmr@meetme.com>
2013-01-23 19:44:57 -0500

    Update the login and logout handlers to reflect new auth in web.py

--- apiary/interface/auth.py
+++ apiary/interface/auth.py
@@ -2,16 +2,19 @@
 Login and Logout Request Handlers
 
 """
-
-from apiary import web
-from apiary.authentication import ldapclient
 import logging
+
+from apiary import authentication
+from apiary import web
 
 LOGGER = logging.getLogger()
 
 
-class Login(web.InterfaceRequestHandler):
+class Login(web.AuthRequestHandler):
+    """Login request handler processes requests for the login form (GET) and
+    authentication requests (POST).
 
+    """
     def authentication_error(self, username):
         """Render the login form with the authentication error"""
         self.render('interface/pages/login.html', username=username,
@@ -22,16 +25,9 @@ class Login(web.InterfaceRequestHandler):
         LOGGER.critical('Could not connect to LDAP server')
         self.render('interface/pages/login.html', connection_error=True)
 
-    @property
-    def ldap_settings(self):
-        """Return the LDAP configuration document.
-
-        :rtype: dict
-
-        """
-        return self.config.get('ldap')
-
     def get(self, *args, **kwargs):
+        if self.session and self.session.authenticated:
+            return self.redirect('/')
         self.render('interface/pages/login.html')
 
     def post(self, *args, **kwargs):
@@ -40,30 +36,28 @@ class Login(web.InterfaceRequestHandler):
         if not username or not password:
             LOGGER.warning('Missing username or password')
             return self.authentication_error(None)
-        settings = self.ldap_settings
         try:
-            user = ldapclient.LDAPClient(settings['uri'],
-                                         username, password,
-                                         settings['base_dn']['people'],
-                                         settings['base_dn']['groups'])
-        except ldapclient.AuthenticationError:
+            user = self._authenticate(username, password)
+        except authentication.AuthenticationError:
             LOGGER.warning('Authentication error for %s', username)
             return self.authentication_error(username)
-        except ldapclient.ConnectionError:
+        except authentication.ConnectionError:
             return self.connection_error()
         self.session.authenticated = True
-        self.session.profile = user.user_profile(username)
-        self.session.groups = user.groups(username)
-
-        if not self.has_group_access(settings['groups']['access']):
+        self.session.profile = user.profile
+        self.session.groups = user.groups
+        if not self.has_group_access(self._ldap_settings['groups']['access']):
             LOGGER.warning('No group access for %s' % username)
             self._clear_session()
             return self.permission_denied()
-
         self.redirect('/')
 
 
-class Logout(web.InterfaceRequestHandler):
+class Logout(web.AuthRequestHandler):
+    """Log the user out of the session, clearing the session and redirecting
+    them to the login page.
+
+    """
     def get(self, *args, **kwargs):
         self._clear_session()
-        self.redirect('/')
+        self.render('interface/pages/login.html', logged_out=True)
