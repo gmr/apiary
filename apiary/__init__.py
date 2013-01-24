19cb000174da20fe6f7f334f39ee8df0ddf12c25
Gavin M. Roy <gmr@meetme.com>
2013-01-23 19:44:14 -0500

    Move the routing UUID regex pattern to the root __init__

--- apiary/__init__.py
+++ apiary/__init__.py
@@ -3,6 +3,6 @@ Apiary is a modern data-center management tool that includes provisioning,
 configuration management and inventory control.
 
 """
-
 __version__ = '0.0.1'
 
+UUID_RE= "(?P<id>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})"
