e647839940d80f30928593a3718650faf21c5aff
Gavin M. Roy <gmr@meetme.com>
2013-01-23 19:44:35 -0500

    Move the UUID regex pattern to the root __init__ removing from API

--- apiary/api/__init__.py
+++ apiary/api/__init__.py
@@ -5,7 +5,7 @@ from apiary.api import breed
 from apiary.api import distribution
 from apiary.api import system
 
-UUID_ID = "(?P<id>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})"
+from apiary import UUID_RE
 
 ROUTES = [("/api/architecture", architecture.Architecture),
           ("/api/architecture/(?P<name>[\w_\-]*)", architecture.Architecture),
@@ -14,8 +14,8 @@ ROUTES = [("/api/architecture", architecture.Architecture),
           ("/api/breed/(?P<name>[\w_\-]*)", breed.Breed),
           ("/api/breeds", breed.Breeds),
           ("/api/distribution", distribution.Distribution),
-          ("/api/distribution/%s" % UUID_ID, distribution.Distribution),
+          ("/api/distribution/%s" % UUID_RE, distribution.Distribution),
           ("/api/distributions", distribution.Distributions),
           ("/api/system", system.System),
-          ("/api/system/%s" % UUID_ID, system.System),
+          ("/api/system/%s" % UUID_RE, system.System),
           ("/api/systems", system.Systems)]
