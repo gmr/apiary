a5ec606071c3a73428f310a4a9012700a5d22c33
Gavin M. Roy <gmr@meetme.com>
2013-01-29 01:42:10 -0500

    Add profile_counts to the distribution model

--- apiary/mappers/distribution.py
+++ apiary/mappers/distribution.py
@@ -3,11 +3,16 @@ Distribution related models
 
 """
 import sqlalchemy
+from sqlalchemy.ext import hybrid
+from sqlalchemy import func
+from sqlalchemy import orm
 import uuid
 
 from apiary.mappers import Base
 from apiary.mappers import architecture
 from apiary.mappers import breed
+from apiary.mappers import profile
+from apiary.mappers import system
 from apiary import types
 
 
@@ -17,6 +22,8 @@ class Distribution(Base):
 
     """
     __tablename__ = 'distributions'
+    __columns__ = ['name', 'version', 'architecture', 'breed', 'profiles']
+    __append__ = ['profile_count']
 
     name = sqlalchemy.Column(sqlalchemy.TEXT, primary_key=True, nullable=False)
     version = sqlalchemy.Column(sqlalchemy.TEXT, nullable=False)
@@ -29,13 +36,24 @@ class Distribution(Base):
     initrd = sqlalchemy.Column(sqlalchemy.TEXT)
     kernel_options = sqlalchemy.Column(sqlalchemy.TEXT)
 
-    def __repr__(self):
+    profiles = orm.relationship("Profile")
+
+    def __reprs__(self):
         """Return the representation of the object
 
         :rtype: str
 
         """
-        return "<Distribution('%s', '%s (%s) %s')>" % (self.id,
-                                                       self.name,
-                                                       self.architecture,
-                                                       self.version)
+        return "<Distribution('%s (%s) %s')>" % (self.name,
+                                                 self.architecture,
+                                                 self.version)
+
+    @hybrid.hybrid_property
+    def profile_count(self):
+        return len(self.profiles)
+
+    @profile_count.expression
+    def profile_count(cls):
+        return (sqlalchemy.select([func.count(profile.Profile.name)]).
+                sqlalchemy.where(profile.Profile.distribution == cls.name).
+                sqlalchemy.label("profile_count"))
--- apiary/mappers/profile.py
+++ apiary/mappers/profile.py
@@ -5,7 +5,6 @@ Profile Model
 import sqlalchemy
 
 from apiary.mappers import Base
-from apiary.mappers import distribution
 from apiary import types
 
 
@@ -17,8 +16,7 @@ class Profile(Base):
     __tablename__ = 'system_profiles'
 
     name = sqlalchemy.Column(sqlalchemy.TEXT, nullable=False, primary_key=True)
-    distro_fk = sqlalchemy.ForeignKey('%s.name' %
-                                      distribution.Distribution.__tablename__)
+    distro_fk = sqlalchemy.ForeignKey('distributions.name')
     distribution = sqlalchemy.Column(types.TEXT, distro_fk, nullable=False)
     parent = sqlalchemy.Column(sqlalchemy.TEXT,
                                sqlalchemy.ForeignKey('system_profiles.name'),
